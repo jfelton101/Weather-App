ed9198ea5d5b1031ed4eca642ecf397f9bcb2438
jfelton101 <jfelton101@users.noreply.github.com>
2020-08-01 17:03:29 -0400

    Added missing curly bracket

--- src/index.js
+++ src/index.js
@@ -64,41 +64,42 @@ function searchCity(event) {
     h1.innerHTML = null;
     alert("Please type in a city");
   }
+}
 
-  let form = document.querySelector("#search-form");
-  form.addEventListener("submit", searchCity);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchCity);
 
-  function currentTemp(response) {
-    let h1 = document.querySelector("h1");
-    let temp = document.querySelector(".currentTemp");
-    let temperature = Math.round(response.data.main.temp);
-    let iconElement = document.querySelector("#icon");
-    let descriptionElement = document.querySelector("#description");
-    let windSpeedElement = document.querySelector("#wind-speed");
-    let humidityElement = document.querySelector("#humidity");
+function currentTemp(response) {
+  let h1 = document.querySelector("h1");
+  let temp = document.querySelector(".currentTemp");
+  let temperature = Math.round(response.data.main.temp);
+  let iconElement = document.querySelector("#icon");
+  let descriptionElement = document.querySelector("#description");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let humidityElement = document.querySelector("#humidity");
 
-    fahrenheitTemperature = response.data.main.temp;
-    temp.innerHTML = temperature;
-    h1.innerHTML = response.data.name;
-    iconElement.setAttribute(
-      "src",
-      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-    descriptionElement.innerHTML = response.data.weather[0].description;
-    windSpeedElement.innerHTML = `Wind Speed: ${Math.round(
-      response.data.wind.speed
-    )} km/h`;
-    humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-  }
+  fahrenheitTemperature = response.data.main.temp;
+  temp.innerHTML = temperature;
+  h1.innerHTML = response.data.name;
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  windSpeedElement.innerHTML = `Wind Speed: ${Math.round(
+    response.data.wind.speed
+  )} km/h`;
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+}
 
-  function displayForecast(response) {
-    let forecastElement = document.querySelector("#forecast");
-    forecastElement.innerHTML = null;
-    let forecast = null;
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
 
-    for (let index = 0; index < 6; index++) {
-      forecast = response.data.list[index];
-      forecastElement.innerHTML += `
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
     <div class="col d-flex justify-content-center">
       <p class="day-one">
         ${formatHours(forecast.dt * 1000)} <br />
@@ -107,21 +108,20 @@ function searchCity(event) {
         }@2x.png" />
         <br />
         <strong>${Math.round(forecast.main.temp_max)}°F</strong> | ${Math.round(
-        forecast.main.temp_min
-      )}°F
+      forecast.main.temp_min
+    )}°F
       </p>
     </div>
   `;
-    }
   }
-  function retrieveCity(city) {
-    let apiKey = "1a83008090457b4fd7f896351fa948c0";
-    let url = `https://api.openweathermap.org/data/2.5/weather?&q=${city}&units=imperial&appid=${apiKey}`;
-    axios.get(url).then(currentTemp);
+}
+function retrieveCity(city) {
+  let apiKey = "1a83008090457b4fd7f896351fa948c0";
+  let url = `https://api.openweathermap.org/data/2.5/weather?&q=${city}&units=imperial&appid=${apiKey}`;
+  axios.get(url).then(currentTemp);
 
-    url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
-    axios.get(url).then(displayForecast);
-  }
+  url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(url).then(displayForecast);
 }
 
 function retrievePosition(position) {
